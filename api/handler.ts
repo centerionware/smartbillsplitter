512f31dfdf710fe78b8d02a1b968edbe489fac21
centerionware <roguestar191@comcast.net>
2025-10-04 06:46:12 +0000

    add screenshots

--- api/handler.ts
+++
@@ -1,9 +0,0 @@
-import { createVercelAdapter } from '../vercel-adapter';
-import { mainHandler } from '../serverless';
-
-/**
- * This is the entrypoint for Vercel.
- * It takes the generic mainHandler and wraps it in the Vercel-specific adapter.
- * Vercel's build system will automatically detect this file in the /api directory.
- */
-export default createVercelAdapter(mainHandler);
